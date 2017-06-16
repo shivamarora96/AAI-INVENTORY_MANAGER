8a8c945fd5c5fc85aed215ce65bc96964f83c3c3
Shivam Arora <shivam_arora@live.in>
2017-06-16 18:44:31 +0530

    Home Activity completed
    Home Activity completed

---
+++ app/src/main/java/com/aai/inventorymanagement/Fragments/DeleteItem.java
@@ -0,0 +1,31 @@
+package com.aai.inventorymanagement.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aai.inventorymanagement.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DeleteItem extends Fragment {
+
+
+    public DeleteItem() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_delete_item, container, false);
+    }
+
+}
